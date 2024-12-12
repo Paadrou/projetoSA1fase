012d7cb02129e842203f5a1de0e25616a68fa3ef
IGOR SILVA SCHIMITZ <igor_s_schimitz@estudante.sesisenai.org.br>
2024-12-11 22:16:28 -0300

    sistema de cadastro funcional

--- script.js
+++ script.js
@@ -1,46 +1,85 @@
 cadastros = []
 
-function cadastrar(){
+function cadastrar() {
 
-    let usuario = gerarUsario ()
-
+    let usuario = gerarUsuario()
+    cadastros.push(usuario)
 
 }
 
-function gerarUsario(){
-    let nome = document.getElementById('inputNome').value.toLowerQuase()
-    let senha = document.getElementById
+function gerarUsuario() {
+
+    let camponome = document.getElementById('inputnome')
+    let camposenha = document.getElementById('inputsenha')
+    let camporepitasenha = document.getElementById('inputrepitasenha')
+
+    let nome = camponome.value.toLowerCase()
+    let senha = camposenha.value
+    let repitasenha = camporepitasenha.value
+
+    usuario = { nome, senha }
+
+    camponome.value = null
+    camporepitasenha.value = null
+    camposenha.value = null
+
+    let pos = cadastros.indexOf(senha, nome)
+    if (nome != null && senha != null) {
+
+        if (pos == -1) {
+
+            if (repitasenha == senha) {
+
+                return usuario
+
+            } else {
+
+                alert('ta errado zé')
+
+            }
+        }
+    }
 }
 
-function paginacadastrar(){
+
+function paginacadastrar() {
 
     esconder()
     document.getElementById("tela1").style.display = 'flex'
 
 }
 
-function paginaeditar(){
+function paginalistar() {
 
     esconder()
-    document.getElementById('tela2').style.display ='flex'
+    document.getElementById("tela2").style.display = 'flex'
+}
+
+function listar() {
+    let convidados = "----------"
+
+    for (let item of cadastros) {
+        convidados += "<br>" + item.toUpperCase() + "<br>----------"
+    }
+    lista.innerHTML = convidados
+}
+
+function paginaeditar() {
+
+    esconder()
+    document.getElementById('tela3').style.display = 'flex'
 
 }
 
-function paginalistar(){
 
-    esconder()
-    document.getElementById("tela3").style.display = 'flex'
-
-}
-
-function paginaexcluir(){
+function paginaexcluir() {
 
     esconder()
     document.getElementById('tela4').style.display = 'flex'
 
 }
 
-function esconder(){
+function esconder() {
 
     document.getElementById('tela1').style.display = 'none'
     document.getElementById('tela2').style.display = 'none'
